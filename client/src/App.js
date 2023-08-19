0ecff30330fee2e9ed279b017c58503e5abc7221
divyyeshchandra <divyyesh.bvp23@gmail.com>
2023-08-19 19:39:07 +0530

    Second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,34 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
-import "./App.css";
-import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 function App() {
-
-  const [currentAccount, setCurrentAccount] = useState('');
+  const [currentAccount, setCurrentAccount] = useState("");
   const [correctNetwork, setCorrectNetwork] = useState(false);
 
-  // Calls Metamask to connect wallet on clicking Connect Wallet button
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window
-
-      if (!ethereum) {
-        console.log('Metamask not detected')
-        return
-      }
-      let chainId = await ethereum.request({ method: 'eth_chainId'})
-      console.log('Connected to chain:' + chainId)
-
-      const rinkebyChainId = '0x4'
-
-      if (chainId !== rinkebyChainId) {
-        alert('You are not connected to the Rinkeby Testnet!')
-        return
-      }
-
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-
-      console.log('Found account', accounts[0])
-      setCurrentAccount(accounts[0])
-    } catch (error) {
-      console.log('Error connecting to metamask', error)
-    }
+  async function connectWallet() {
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let accounts = await provider.send("eth_requestAccounts");
+    setCurrentAccount(accounts);
+    setCorrectNetwork(true);
+    let signer = provider.getSigner();
+    console.log("Account address:", await signer.getAddress());
   }
-
-  // Checks if wallet is connected to the correct network
-  const checkCorrectNetwork = async () => {
-    const { ethereum } = window
-    let chainId = await ethereum.request({ method: 'eth_chainId' })
-    console.log('Connected to chain:' + chainId)
-
-    const rinkebyChainId = '0x4'
-
-    if (chainId !== rinkebyChainId) {
-      setCorrectNetwork(false)
-    } else {
-      setCorrectNetwork(true)
-    }
-  }
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    connectWallet();
-    checkCorrectNetwork();
-  });
 
   return (
-    // BEM
     <div>
-    {currentAccount === '' ? (
-      <button
-      className='text-2xl font-bold py-3 px-12 bg-[#f1c232] rounded-lg mb-10 hover:scale-105 transition duration-500 ease-in-out'
-      onClick={connectWallet}
-      >
-      Connect Wallet
-      </button>
+      {currentAccount === "" ? (
+        <div className="MainPage">
+          <button
+            style={{ borderRadius: "50px", height: "50px", width: "400px" }}
+            onClick={connectWallet}
+          >
+            Connect Wallet
+          </button>
+        </div>
       ) : correctNetwork ? (
         <div className="app">
           <Sidebar />
@@ -76,15 +36,14 @@ function App() {
           <Widgets />
         </div>
       ) : (
-      <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
-      <div>----------------------------------------</div>
-      <div>Please connect to the Rinkeby Testnet</div>
-      <div>and reload the page</div>
-      <div>----------------------------------------</div>
-      </div>
-    )}
+        <div className="flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3">
+          <div>----------------------------------------</div>
+          <div>Please connect to the Rinkeby Testnet</div>
+          <div>and reload the page</div>
+          <div>----------------------------------------</div>
+        </div>
+      )}
     </div>
-
   );
 }
 
